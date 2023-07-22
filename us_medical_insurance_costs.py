34a8a94c25c1c1ba89707bab695475670d19fc33
Noah A Klem <noahaklem@gmail.com>
2023-07-22 09:08:57 -0600

    import necessary libraries, setup lists for data

--- us_medical_insurance_costs.py
+++ us_medical_insurance_costs.py
@@ -1,1 +1,22 @@
 #inital setup/commit is left blank on purpose for start of project 
+
+# Project Scope:
+# Project is filled with a data sample of medical insurance participates
+# Step 0: What is the problem? Understand various attributes inside data about patient information
+# Step 1: Goals - Complete all necessary pieces and answer any questions about dataset
+# Step 2: Actions - No actions needed
+# Step 3: Data - Data provided. Needed a larger sample size for analysis
+# Step 4: Analysis - Description and detection
+# Additional Considerations - 
+
+import csv
+
+# numerical and categorical values these are the variables, create empty lists to store data
+ages = []
+sexes = []
+bmis = []
+num_children = []
+smoker_statuses = []
+regions = []
+insurance_charges = []
+
